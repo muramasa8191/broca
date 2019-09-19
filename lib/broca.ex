ec972b79725e37fb5cf470f3d2ce6d6f3d9570fd
Masayoshi Tamura <muramasa8191@gmail.com>
2019-09-19 17:07:16 +0900

    Add missing end.

--- lib/broca.ex
+++ lib/broca.ex
@@ -4,23 +4,24 @@ defmodule Broca do
   """
 
   def ch05(iterate, batch_size) do
-    {x_train, t_train} = Broca.Dataset.MNIST.load_train
+    {x_train, t_train} = Broca.Dataset.MNIST.load_train()
     {model, loss_layer} = Broca.Models.TwoLayerNet.new(784, 50, 10)
-    
+
     zip_data = Enum.zip(x_train, t_train)
 
     for i <- 1..iterate do
-      {x_batch, t_batch} = 
-        zip_data 
-        |> Enum.shuffle 
-        |> Enum.take(batch_size) 
-        |> Enum.unzip
-      
+      {x_batch, t_batch} =
+        zip_data
+        |> Enum.shuffle()
+        |> Enum.take(batch_size)
+        |> Enum.unzip()
+
       {model2, out} = Broca.Models.TwoLayerNet.predict(model, x_batch)
       acc = Broca.Models.TwoLayerNet.accuracy(out, t_batch)
       IO.puts("Accuracy: #{acc}")
 
       {model3, _} = Broca.Models.TwoLayerNet.gradient(model2, loss_layer, x_batch, t_batch)
       model = Broca.Models.TwoLayerNet.update(model3)
+    end
   end
 end
